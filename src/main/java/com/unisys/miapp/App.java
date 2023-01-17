eaa5af647ee9a0e7df99c1ad932e21d8cb6ff086
kalamardo1989 <kala.mormar@gmail.com>
2023-01-17 16:16:25 +0100

    Initial commit

---
+++ src/main/java/com/unisys/miapp/App.java
@@ -0,0 +1,48 @@
+package com.unisys.miapp;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Bucle while hasta 10");
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+
+        System.out.println("Bucle DO while hasta 10");
+        int j = 0;
+        do {
+            System.out.println(j);
+            j++;
+        } while (j < 10);
+
+
+        System.out.println("Bucle For hasta 10");
+        int z = 0;
+        for (z = 0; z < 10;z++){
+            System.out.println(z);
+        }
+
+        int edadPersona=21;
+                if (edadPersona<18){
+                    System.out.println("Edad "+edadPersona+" No puede condiducir");
+                }else{
+                    System.out.println("Edad "+edadPersona+" Puede condiducir");
+                }
+
+        switch(edadPersona) {
+            case 18:
+                System.out.println("Puede conducir");
+                break;
+            case 21:
+                System.out.println("Puede beber en USA");
+                break;
+            default:
+                System.out.println("Su edad es:" + edadPersona);
+                break;
+        }
+    }
+
+
+}
